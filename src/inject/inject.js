1a3f6cbb3ccaadcb56d7e2fef559ef74a86b223b
Naqushab Neyazee <neyazee@adobe.com>
2020-06-23 10:33:48 +0530

    bring back the download button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,45 +4,34 @@
 var addDownloadButtonToAllSongs = function() {
 
     $('.single-download-button').remove();
+    var songsEle = $('figcaption').find('a.u-color-js-gray');
 
-    $('.song-json').each(function() {
+    songsEle.each(function() {
         var $this = $(this);
-        var btn = $('<a class="single-download-button btn x-small"></a>');
+        var btn = $('<div class="o-snippet__item single-download-button"><span class="u-link"><i class="o-icon--large u-pop-in o-icon-download"></i></span></div>');
         try {
-            var song = JSON.parse($this.text());
+            var songLink = this.href;
+            console.log('Song link: ' + this);
         } catch (e) {}
-
-
-        btn.text('Download').css({
-            marginLeft: '7px',
-            fontSize: '8px',
-            padding: '1px 11px',
-            background: '#E91E63'
-        });
 
         btn.on('click', function(e) {
             e.preventDefault();
             var $btn = $(this);
-            $btn.text('Downloading....');
-
+            
             getDownloadURL(song, false, function(result, status) {
-
-
                 if (status === 'success') {
-                    downloadWithData(song, result.auth_url, function() {
-                        $btn.text('Download');
+                    downloadWithData(songLink, result.auth_url, function() {
+                        $btn.addClass('o-icon-download-fill u-color-js-blue');
                     });
                 }
                 if (status === 'error') {
-                    $btn.text('Error in Download');
-
+                    $btn.removeClass('o-icon-down-fill u-color-js-blue');
+                    console.log('Failed to download song' + $this.href)
                 }
-
             });
         });
 
-
-        $this.parent().find('.main .title').first().append(btn);
+        $this.parent().parent().parent().parent().parent().last().append(btn);
     });
 };
 
@@ -50,19 +39,17 @@ var addDownloadButtonToAllSongs = function() {
  * Add Album Download Button on Albums
  */
 var addAlbumDownloadButton = function() {
-    $('.album-download').remove();
 
+    $('.album-download').remove();
     var albumBtn = $('<li><a class="album-download btn white outline">Download</a></li>');
 
     albumBtn.on('click', function() {
-
         var songs = [];
         $('.song-json').each(function() {
             songs.push(JSON.parse($(this).text()))
         });
 
         downloadSetOfSongsAsZip(songs, songs[0].album + bitrateString);
-
     });
 
     $('.album-view section .page-header .actions').prepend(albumBtn);
@@ -74,18 +61,15 @@ var addAlbumDownloadButton = function() {
 var addPlaylistDownloadButton = function() {
 
     $('.playlist-download').remove();
-
     var playlistBtn = $('<li><a class="playlist-download btn">Download</a></li>');
 
     playlistBtn.on('click', function() {
-
         var songs = [];
         $('.song-json').each(function() {
             songs.push(JSON.parse($(this).text()))
         });
 
         downloadSetOfSongsAsZip(songs, $('.page-title').text() + bitrateString);
-
     });
 
     $('.playlist .page-header .actions').prepend(playlistBtn);
@@ -104,26 +88,19 @@ var createDownloadQuality = function() {
 
     menuItem.find('.curr-down-rate').first().text(localStorage.download_bitrate + " kbps");
     bitrates = bitrates.map(function(rate) {
-
-
         var el = $('<li class="o-list-select__item" ><a>' + rate + ' kbps</a></li>');
-
         if (rate === localStorage.download_bitrate) {
             el.addClass('selected');
         }
-
         el.on('click', function() {
             localStorage.download_bitrate = rate;
             $(this).parent().find('.selected').each(function() {
                 $(this).removeClass('selected');
                 $(this).find('a em').remove();
             });
-
             $(this).addClass('selected');
-
             menuItem.removeClass('active');
             menuItem.find('.curr-down-rate').first().text(localStorage.download_bitrate + ' kbps');
-
         });
         return el;
     });
@@ -146,6 +123,7 @@ var createDownloadQuality = function() {
  * Run on Plugin Initialization
  */
 var initPlugin = function() {
+    console.log("Initialize the Saavn Extension");
     addDownloadButtonToAllSongs();
     addAlbumDownloadButton();
     addPlaylistDownloadButton();
@@ -154,6 +132,7 @@ var initPlugin = function() {
 };
 
 var hideAds = function() {
+    console.log("Ads Hidden Now");
     $('.ad').remove();
     $('.c-ad').remove();
     $('.c-banner').remove();
@@ -162,29 +141,24 @@ var hideAds = function() {
 $(document).ready(function() {
 
     hideAds();
-    console.log("Ads Hidden Now");
 
     setTimeout(function() {
-        console.log("Initialize the plugin");
         initPlugin();
     }, 2000);
 
     createDownloadQuality();
 
     // check if classes of the .page-wrap changes then add the buttons again
-    var OldLen = 0;
+    var oldSongListLen = 0;
     var inter = setInterval(function() {
-        if ($('.page-wrap').length) {
-            if ($('.song-wrap').length) {
-                var len = $('.song-wrap').length;
-
-                if (len !== OldLen) {
-                    console.log("Song List is changed. Initilizing plugin again.")
-                    initPlugin();
-                }
-                OldLen = len;
+        if ($('ol.o-list-bare').find('li').length) {
+            var songListLen = $('ol.o-list-bare').find('li').length;
+            if (songListLen !== oldSongListLen) {
+                console.log("Song List is changed. Initilizing plugin again.")
+                initPlugin();
             }
+            oldSongListLen = songListLen;
         }
-    }, 1000);
+    }, 2000);
 
 });
